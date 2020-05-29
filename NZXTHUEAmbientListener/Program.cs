6fcab9cd1d68a8f8dd0c14e16c928e1a9b6a8e66
Cheerpipe <cheerpipe@gmail.com>
2020-05-28 23:00:27 -0400

    New icon, low priority process and single running instance

--- NZXTHUEAmbientListener/Program.cs
+++ NZXTHUEAmbientListener/Program.cs
@@ -3,27 +3,36 @@ using Microsoft.Win32;
 using NZXTHUEAmbient;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-
+using System.Threading;
 
 namespace NZXTHUEAmbientListener
 {
     // For use with Aurora Project Scripted Device or any other program using NamedPipes
     // https://github.com/antonpup/Aurora 
 
-    static class Program
+    internal static class Program
     {
+        internal static Mutex _singleInstanceMutex = new Mutex(true, "{7073d39a-532d-4b06-bde3-19732814ee77}");
+
         private static List<Listener> _listeners = new List<Listener>();
         static void Main()
         {
+            //No more than one instance runing
+            if (!_singleInstanceMutex.WaitOne(TimeSpan.Zero, true))
+                return;
+
+            Util.SetPriorityProcessAndThreads(Process.GetCurrentProcess().ProcessName, ProcessPriorityClass.Idle, ThreadPriorityLevel.Lowest);
+
+            SystemEvents.SessionEnding += SystemEvents_SessionEnding;
+
             HUE2AmbientDeviceLoader.InitDevices().Wait();
 
             if (HUE2AmbientDeviceLoader.Devices.Length == 0)
             {
                 throw new Exception("No HUE 2 Ambiente devices found");
             }
-
-            SystemEvents.SessionEnding += SystemEvents_SessionEnding;
 
             for (int c = 0; c < HUE2AmbientDeviceLoader.Devices.Length; c++)
             {
